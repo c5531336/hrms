d40ce01f763e57c28132aba73175975ce84d06e5
max26292 <max26292@yahoo.com>
2019-11-04 21:58:57 +0700

    update layout

--- resources/views/layouts/authorize.blade.php
+++ resources/views/layouts/authorize.blade.php
@@ -1,1 +1,20 @@
-<?php
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Đăng Nhập</title>
+    <link rel="stylesheet" href="{{asset('assets/css/library.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/css/app.css')}}">
+    @yield('css-file')
+</head>
+<body class="login-page">
+    @yield('content')
+<script src="{{asset('assets/js/app.js')}}"></script>
+<script src="{{asset('assets/js/library.min.js')}}"></script>
+{{--    demo--}}
+@yield('script')
+<script src="{{asset('assets/js/dashboard3.js')}}"></script>
+</body>
+</html>
--- resources/views/partials/control-sidebar.blade.php
+++ resources/views/partials/control-sidebar.blade.php
@@ -1,1 +1,5 @@
-<?php
+<!-- Control Sidebar -->
+<aside class="control-sidebar control-sidebar-dark">
+    <!-- Control sidebar content goes here -->
+</aside>
+<!-- /.control-sidebar -->
