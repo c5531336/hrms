6a3920e1a595ee718bb641ebd6b6a479384d59f1
max26292 <max26292@yahoo.com>
2019-11-28 21:39:25 +0700

    update edit view

--- resources/views/Branch/edit.blade.php
+++ resources/views/Branch/edit.blade.php
@@ -2,6 +2,53 @@
 @section('content')
 	<div class="content-wrapper">
 		<section class="content">
-		</section>
+            @extends('layouts.app')
+            @section('content')
+                <div class="content-wrapper">
+                    <section class="content">
+                        <div class="card card-primary">
+                            <div class="card-header">
+                                <h3 class="card-title">Sửa chi nhánh</h3>
+                            </div>
+                            <!-- /.card-header -->
+                            <!-- form start -->
+                            <form role="form" action="{{route('branch.update',$branch->BranchId)}}" method="POST">
+                                @method('PUT')
+                                @csrf
+                                <div class="card-body">
+                                    <div class="form-group">
+                                        <label for="BranchCode">Mã Chi Nhánh</label>
+                                        <input type="text" class="form-control" name="BranchCode" placeholder="VL"
+                                               value="{{$branch->BranchCode}}"/>
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="Name">Tên Chi Nhánh</label>
+                                        <input type="text" class="form-control" name="Name" placeholder="Vĩnh Lộc"
+                                               value="{{$branch->Name}}"/>
+                                    </div>
+                                </div>
+                                <!-- /.card-body -->
+                                <div class="card-footer">
+                                    <button type="submit" class="btn btn-primary">Sửa </button>
+                                </div>
+                            </form>
+                        </div>
+                    </section>
+                </div>
+            @endsection
+            @if($errors &&($errors->has('BranchCode')|| $errors->has('Name')))
+            @section('script')
+                <script>
+                    toastr.options.showDuration=800;
+                    $(document).ready(function () {
+                        @foreach ($errors->all() as $message)
+                        toastr.error('{{$message}}');
+                        @endforeach
+                    })
+                </script>
+            @endsection
+            @endif
+
+        </section>
 	</div>
 @endsection
